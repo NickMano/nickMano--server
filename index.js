20daeab3abf9061948e80877d3829482f5c3416e
Nico <nmanograsso@gmail.com>
2021-01-19 11:13:08 -0300

    add cors package to enable Cross-Origin Resource Sharing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { config } = require('./config/index');
 const projectsApi = require('./routes/projects');
 const {
@@ -12,6 +13,7 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // routes
 projectsApi(app);
